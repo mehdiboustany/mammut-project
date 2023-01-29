877b7f0724dedf428e0878ee7176846cfb501810
Mehdi Boustany <66678871+mehdiboustany@users.noreply.github.com>
2023-01-29 16:48:58 +0330

    Delete template-parts/partials/sales directory

--- template-parts/partials/sales/hero.php
+++
@@ -1,7 +0,0 @@
-<section class="o-section c-section--hero" style="background: url(<?php echo get_template_directory_uri(); ?>/assets/images/sale.webp); background-position: center; background-size: cover;">
-    <div class="o-section__wrapper">
-        <div class="c-hero">
-            <h1 class="c-hero__title">فـــــروش پروژه های مسکونی، تجــــــاری و تفریحی لوکس</h1>
-        </div>
-    </div>
-</section>
