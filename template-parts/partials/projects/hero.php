80debd21af91dc13aeb07804cb5f05697127e927
Mehdi Boustany <66678871+mehdiboustany@users.noreply.github.com>
2023-01-29 16:48:52 +0330

    Delete template-parts/partials/projects directory

--- template-parts/partials/projects/hero.php
+++
@@ -1,7 +0,0 @@
-<section class="o-section c-section--hero" style="background: url(<?php echo get_template_directory_uri(); ?>/assets/images/bg-projects.webp); background-position: center; background-size: cover;">
-    <div class="o-section__wrapper">
-        <div class="c-hero">
-            <h1 class="c-hero__title">پروژه ی ویلاهای لوکس رامســـــر</h1>
-        </div>
-    </div>
-</section>
